1628448d94f9c0a2b02ca3f4244f5f35ce79eb81
User <user@example.com>
2025-05-07 11:19:12 -0700

    added app

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import OrderOfOperations from './components/OrderOfOperations'
+
+
+function App() {
+  return (
+      <div className="App">
+        <OrderOfOperations />
+      </div>
+  );
+}
+
+
+export default App;
---
+++ src/components/OrderOfOperations.jsx
@@ -0,0 +1,13 @@
+import React, { useState, useEffect, useRef } from 'react';
+
+
+const OrderOfOperations = () => {
+
+
+	return (
+        <div></div>
+)
+};
+
+
+export default OrderOfOperations;
